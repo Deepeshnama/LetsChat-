09750999ce956e2fb318a793cef0917f2f4472ed
Deepeshnama <deepeshnama0786@gmail.com>
2024-12-20 00:51:51 +0530

    added

--- backend/backend/routes/user.routes.js
+++ backend/backend/routes/user.routes.js
@@ -1,11 +1,9 @@
 import express from "express";
-import { getAllUsers, login, register } from "../controllers/user.controller.js";
+import { login, register } from "../controllers/user.controller.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", register);
 userRouter.post("/login", login);
-
-userRouter.get("/" , getAllUsers )
 
 export default userRouter;
